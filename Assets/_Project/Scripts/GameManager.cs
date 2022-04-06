bb6e7d6beafaa4434bf15f8aa712c314cfdb8a83
ayutaz <ka1357amnbpdr@gmail.com>
2022-04-07 00:46:59 +0900

    feat: get json and png from ccd

---
+++ Assets/_Project/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+using System.IO;
+using Cysharp.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+namespace _CCD_Test
+{
+    public class GameManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        [SerializeField] private MyData myData;
+        private const string JsonURL = "generated.json";
+        private const string PNGURL = "snapshot.png";
+        private const string FBXURL = "PT_Fruit_Tree_01_green.fbx";
+        [SerializeField] private RawImage rawImage;
+        [SerializeField] private TextMeshProUGUI jsonText;
+
+        private void Start()
+        {
+            GetJson();
+            GetPNG();
+            GetFBX();
+        }
+
+        private void GetFBX()
+        {
+        }
+
+        private async void GetPNG()
+        {
+            var texture = UnityWebRequestTexture.GetTexture(Path.Combine(myData.ccdBaseURL, PNGURL));
+            await texture.SendWebRequest();
+            rawImage.texture = ((DownloadHandlerTexture)texture.downloadHandler).texture;
+        }
+
+        private async void GetJson()
+        {
+            var json = UnityWebRequest.Get(Path.Combine(myData.ccdBaseURL, JsonURL));
+            await json.SendWebRequest();
+            jsonText.text = json.downloadHandler.text;
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/MyData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace _CCD_Test
+{
+    [CreateAssetMenu(fileName = "MyData", menuName = "MyData", order = 0)]
+    public class MyData : ScriptableObject
+    {
+        public string ccdBaseURL;
+    }
+}
